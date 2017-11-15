3b5e1c700e6fcd67e26f1f972a03cdac476c1a06
gauravpatel2016 <gauravpatel2016@gmail.com>
2017-11-15 12:47:43 -0700

    Add Pact Java Example (#1)

---
+++ pact/java/provider/src/main/java/bridge/Bridge.java
@@ -0,0 +1,21 @@
+package bridge;
+
+public class Bridge {
+
+    private final long id;
+    private final String content;
+
+    public Bridge(long id, String content) {
+        this.id = id;
+        this.content = content;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+}
---
+++ pact/java/provider/src/main/java/bridge/BridgeConfiguration.java
@@ -0,0 +1,13 @@
+package bridge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BridgeConfiguration {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BridgeConfiguration.class, args);
+    }
+
+}
