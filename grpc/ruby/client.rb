faf5bf9b9fc9c8550c787fccea90ab262a146c00
Michael Hargiss <mdhargiss@gmail.com>
2019-05-06 13:32:26 -0400

    remove grpc examples

--- grpc/ruby/client.rb
+++
@@ -1,9 +0,0 @@
-$LOAD_PATH.unshift __dir__
-require 'soseedy_services_pb'
-
-stub = Soseedy::SoSeedy::Stub.new('localhost:50051', :this_channel_is_insecure)
-teacher = stub.create_teacher(Soseedy::CreateTeacherRequest.new)
-
-puts "Teacher id: #{teacher.id}"
-puts "Teacher loginId: #{teacher.username}"
-puts "Teacher name: #{teacher.name}"
